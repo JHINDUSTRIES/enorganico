f10cc79359f3b272c262f66894d9608b8c5e3ff5
camiicode <dftp93@gmail.com>
2026-01-12 23:46:42 -0500

    [1201202605] Add typescript for .mov file into astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,10 +10,12 @@ export default defineConfig({
     css: {
       postcss: {},
     },
+    // ESTA ES LA PARTE NUEVA ↓↓↓
+    assetsInclude: ["**/*.mov", "**/*.mp4", "**/*.webm", "**/*.avi", "**/*.mkv"],
   },
-  site: 'https://camiicode.github.io',
-  base: '/enorganico',
+  site: "https://camiicode.github.io",
+  base: "/enorganico",
   build: {
-    assets: 'assets'
-  }
+    assets: "assets",
+  },
 });
---
+++ src/env.d.ts
@@ -0,0 +1,16 @@
+/// <reference types="astro/client" />
+
+declare module "*.mov" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.mp4" {
+  const src: string;
+  export default src;
+}
+
+declare module "*.webm" {
+  const src: string;
+  export default src;
+}
